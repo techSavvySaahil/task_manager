9dd1dc27d5d3017032972488f8a8f2bf187d8baa
techSavvySaahil <saahil.madaan@gmail.com>
2019-01-10 04:13:47 +0530

    Updates

---
+++ precache-manifest.5486ffaf2b150c46e9b92e3424480bb7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7519a2a2b22edb3e1d0c",
+    "url": "/task_manager/static/js/runtime~main.7519a2a2.js"
+  },
+  {
+    "revision": "0142fad47c27748a2228",
+    "url": "/task_manager/static/js/main.0142fad4.chunk.js"
+  },
+  {
+    "revision": "9370782dd8de3ac6d8d3",
+    "url": "/task_manager/static/js/1.9370782d.chunk.js"
+  },
+  {
+    "revision": "0142fad47c27748a2228",
+    "url": "/task_manager/static/css/main.0612e9e8.chunk.css"
+  },
+  {
+    "revision": "87aab70db16ca40e470b8f95a3b049ac",
+    "url": "/task_manager/index.html"
+  }
+];
